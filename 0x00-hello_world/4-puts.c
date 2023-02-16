aeaf9c1f98b5a51007a21c6a5e0dee1e57df5ec0
HeinessPremium <heinesspremium@github.com>
2023-02-16 13:44:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - this is program prints a line with puts function
+ * main - this program prints a line with puts function
  *
  * Return: always 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle);
-	return (0):
+	return (0);
 }
