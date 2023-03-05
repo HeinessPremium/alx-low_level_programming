6c168b841d3ef4ca7d62891c59d459e9b71ff2b9
HeinessPremium <heinesspremium@github.com>
2023-03-05 12:41:10 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#include "main.h"
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-int _isdigit(int c);
+int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -11,3 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
+#endif
